f7a45c43fda6301bb36a5dd51bf4be2c9cfa5a10
trum <bardabarda228@gmail.com>
2023-01-02 00:25:06 +0500

    Init

---
+++ Web3BalanceChecker/Models/AddressInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Web3BalanceChecker.Models
+{
+    public class AddressInfo
+    {
+        public string Address { get; set; }
+        public double Ethereum { get; set; }
+        public double BNBChain { get; set; }
+        public double Optimism { get; set; }
+        public double Polygon { get; set; }
+        public double Fantom { get; set; }
+        public double Arbitrum { get; set; }
+        public double Avalanche { get; set; }
+        public double Total { get; set; }
+        
+        public AddressInfo () {}
+
+        public AddressInfo(string address, List<double> balanceList, double total)
+        {
+            Address = address;
+            Ethereum = balanceList[0];
+            BNBChain = balanceList[1];
+            Optimism = balanceList[2];
+            Polygon = balanceList[3];
+            Fantom = balanceList[4];
+            Arbitrum = balanceList[5];
+            Avalanche = balanceList[6];
+            Total = total;
+        }
+    }
+}
---
+++ Web3BalanceChecker/ViewModels/ViewModel.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Web3BalanceChecker.Annotations;
+using Web3BalanceChecker.Commands;
+using Web3BalanceChecker.Models;
+
+namespace Web3BalanceChecker
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private BackgroundWorker _bw;
+        private List<int> ChainIdList = new() { 1, 56, 10, 137, 250, 42161, 43114  };
+        public ObservableCollection<AddressInfo> AddressInfos { get; set; }
+        public ObservableCollection<string> ReadAddresses { get; set; }
+
+        public ViewModel()
+        {
+            AddressInfos = new ObservableCollection<AddressInfo>();
+            ReadAddresses = new ObservableCollection<string>();
+            _bw = new BackgroundWorker()
+            {
+                WorkerReportsProgress = true
+            };
+            _bw.DoWork += BwOnDoWork;
+            _bw.ProgressChanged += BwOnProgressChanged;
+            _bw.RunWorkerCompleted += BwOnRunWorkerCompleted;
+        }
+
+        private void BwOnRunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            CommandManager.InvalidateRequerySuggested();
+            ProgressState = 0;
+        }
+
+        private void BwOnProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            if ((AddressInfo)e.UserState != null)
+            {
+                AddressInfos.Add((AddressInfo)e.UserState);
+            }
+
+            ProgressState = e.ProgressPercentage;
+        }
+
+        private void BwOnDoWork(object? sender, DoWorkEventArgs e)
+        {
+            var bw = (BackgroundWorker) sender;
+            for (int i = 0; i < ReadAddresses.Count; i++)
+            {
+                Debug.WriteLine(ReadAddresses[i]);
+                var line = ReadAddresses[i].Trim();
+                if (line.Length < 42)
+                    continue;
+                var address = "";
+                if (line.Contains(':'))
+                {
+                    var parts = line.Split(':');
+                    line = parts.FirstOrDefault((x) => x.Length is 42 or 66 && x.StartsWith("0x"))?.ToString();
+                }
+                address = line.Length is 42 ? line : new Nethereum.Web3.Accounts.Account(line).Address;
+                
+                HttpClientHandler handler = new HttpClientHandler();
+                handler.AutomaticDecompression = DecompressionMethods.All;
+
+                var client = new HttpClient(handler);
+
+                var balanceList = new List<double>();
+                
+                foreach (var request in ChainIdList.Select(chainId => new HttpRequestMessage(HttpMethod.Get, $"https://account.metafi.codefi.network/accounts/{address.ToLower()}?chainId={chainId}&includePrices=true")))
+                {
+                    Debug.WriteLine("test");
+                    request.Headers.Add("referer", "https://portfolio.metamask.io/");
+
+                    var response = client.SendAsync(request).Result;
+                    response.EnsureSuccessStatusCode();
+                    var responseBody = response.Content.ReadAsStringAsync().Result;
+                    var responseJson = JObject.Parse(responseBody);
+                    double balance = 0;
+                    try
+                    {
+                        balance = Math.Round((double)responseJson["value"]["marketValue"], 2);
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception);
+                    }
+                    
+                    balanceList.Add(balance);
+                }
+
+                var total = balanceList.Sum();
+
+                var addressInfo = new AddressInfo(address, balanceList, total);
+                bw.ReportProgress((int)((i + 1) / (float)ReadAddresses.Count * 100), addressInfo);
+            }
+        }
+
+        private int _progress;
+        public int ProgressState
+        {
+            get => _progress;
+            set
+            {
+                _progress = value;
+                OnPropertyChanged(nameof(ProgressState));
+            }
+        }
+
+        public ICommand LoadDataFromFileCommand =>
+            new RelayCommand(() =>
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.InitialDirectory = "C:\\";
+                dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                dialog.FilterIndex = 2;
+                dialog.RestoreDirectory = true;
+
+                if (dialog.ShowDialog() == true)
+                {
+                    var fs = dialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fs))
+                    {
+                        string line;
+                        while (true)
+                        {
+                            line = reader.ReadLine();
+                            if (line != null)
+                            {
+                                ReadAddresses.Add(line.Trim('\n', '\r'));
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }, () => true);
+
+        public ICommand ScanAddresses =>
+            new RelayCommand(() =>
+            {
+                _bw.RunWorkerAsync(this);
+            }, () => true);
+    }
+}
